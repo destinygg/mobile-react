e5baa6754b18f965ad87e2c54c087f3577cdd03b
David Cako <dc@cako.io>
2017-11-13 11:45:48 -0700

    crash reporting

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { Client } from 'bugsnag-react-native';
+
+const bugsnag = new Client();
 
 AppRegistry.registerComponent('destinyapp', () => App);
