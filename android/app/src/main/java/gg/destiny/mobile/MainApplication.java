95cfd55ee3375178a544337f1cdcaa3bf210ceed
David Cako <dc@cako.io>
2017-12-12 10:26:54 -0700

    unlink touch-through-view

--- android/app/src/main/java/gg/destiny/mobile/MainApplication.java
+++ android/app/src/main/java/gg/destiny/mobile/MainApplication.java
@@ -3,7 +3,6 @@ package gg.destiny.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.rome2rio.android.reactnativetouchthroughview.TouchThroughViewPackage;
 import com.bugsnag.BugsnagReactNative;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new TouchThroughViewPackage(),
             BugsnagReactNative.getPackage()
       );
     }
